9c1f26727d315462f09694a1d0c354b070e33ee2
jd-d <jd-d@users.noreply.github.com>
2025-10-18 16:18:13 +0000

    deploy: 881d8f23e0e74abc6505dc303e4ae83d43929f4a

---
+++ previews/pr-27/assets/js/constants.js
@@ -0,0 +1,6 @@
+export const WEEKS_PER_YEAR = 52;
+export const MONTHS_PER_YEAR = 12;
+export const BASE_WORK_DAYS_PER_WEEK = 7;
+export const TARGET_NET_DEFAULT = 50000;
+
+export const TARGET_NET_BASIS_VALUES = ['year', 'week', 'month', 'avgWeek', 'avgMonth'];
